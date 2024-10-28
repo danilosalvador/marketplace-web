6ad13aa77ceb34f9585373ce33ca5d509abda8fc
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-10-28 18:07:14 -0300

    Tela de Login

--- src/pages/_layouts/public.tsx
+++ src/pages/_layouts/public.tsx
@@ -1,10 +1,28 @@
 import { Outlet } from 'react-router-dom'
 
+import logo from '../../assets/logo.svg'
+import illustration from '../../assets/illustration.png'
+
 export function PublicLayout() {
   return (
-    <div>
-      <h1>PublicLayout</h1>
-      <Outlet />
+    <div className="min-h-screen grid grid-cols-2 bg-background">
+      <div className="h-full flex flex-col">
+        <div className="flex items-center gap-5 p-10">
+          <img src={logo} alt="" />
+          <div className="flex flex-col">
+            <span className="font-title text-title-md font-fw-title text-gray-500">
+              Marketplace
+            </span>
+            <span className="font-text text-body-md text-gray-400">
+              Painel de Vendedor
+            </span>
+          </div>
+        </div>
+        <img src={illustration} alt="" />
+      </div>
+      <div className="flex flex-col items-center justify-center bg-white rounded-[32px] m-6 px-20 py-[4.5rem]">
+        <Outlet />
+      </div>
     </div>
   )
 }
--- src/pages/public/sign-in.tsx
+++ src/pages/public/sign-in.tsx
@@ -1,3 +1,51 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
-  return <h1>SignIn</h1>
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="flex flex-col flex-1 items-center justify-between w-96">
+        <div className="flex flex-col gap-9 w-full">
+          <div className="flex flex-col gap-1">
+            <h1 className="font-title text-title-md font-fw-title tracking-tight text-gray-500">
+              Acesse sua conta
+            </h1>
+            <p className="font-text text-body-sm text-gray-300">
+              Informe seu e-mail e senha para entrar
+            </p>
+          </div>
+          <form className="flex flex-col gap-12">
+            <div className="space-y-2">
+              <div className="space-y-2">
+                <Label htmlFor="email">E-mail</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="Seu e-mail cadastrado"
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Senha</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Sua senha de acesso"
+                />
+              </div>
+            </div>
+            <Button type="submit">Acessar</Button>
+          </form>
+        </div>
+        <div className="flex flex-col gap-5 w-full">
+          <span className="font-text text-body-md text-gray-300">
+            Ainda não tem conta?
+          </span>
+          <Button variant="outline">Cadastrar</Button>
+        </div>
+      </div>
+    </>
+  )
 }
